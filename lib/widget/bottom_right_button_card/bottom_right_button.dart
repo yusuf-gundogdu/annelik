ad11e28e91b150bbe7b97b47d097194575cc93d0
yusufgundogdu555 <96109003+yusufgundogdu555@users.noreply.github.com>
2022-07-19 19:22:09 +0300

    dd

--- lib/widget/bottom_right_button_card/bottom_right_button.dart
+++ lib/widget/bottom_right_button_card/bottom_right_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import '../../utils/color.dart';
 import 'drawing.dart';
 
 class BottomRightBottomCard extends StatelessWidget {
@@ -31,31 +32,24 @@ class BottomRightBottomCard extends StatelessWidget {
               ),
             ),
           ),
-          // Shimmer.fromColors(
-          //   baseColor: Colors.grey[300]!,
-          //   highlightColor: Colors.grey[100]!,
-          //   child: Card(
-          //     margin: EdgeInsets.zero,
-          //     elevation: 0.0,
-          //     shape: RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.circular(50),
-          //     ),
-          //     child: SizedBox(
-          //       height: objectSize * 0.13,
-          //       width: objectSize * 0.13,
-          //       child: FittedBox(
-          //         child: ElevatedButton(
-          //           style: ElevatedButton.styleFrom(elevation: 0, shape: const CircleBorder(), backgroundColor: black12),
-          //           child: Container(
-          //             width: 200,
-          //             height: 200,
-          //             alignment: Alignment.center,
-          //             decoration: const BoxDecoration(shape: BoxShape.circle),
-          //             child: Image.asset(imagePath, color: white),
-          //           ),
-          //           onPressed: () {},
-          //         ),
+          // CustomPaint(
+          //   size: Size(objectSize.toDouble(), (objectSize * 0.42857142857142855).toDouble()),
+          //   painter: RPSCustomPainter(buttonBackGroundColor),
+          // ),
+          // SizedBox(
+          //   height: objectSize * 0.13,
+          //   width: objectSize * 0.13,
+          //   child: FittedBox(
+          //     child: ElevatedButton(
+          //       style: ElevatedButton.styleFrom(elevation: 0, shape: const CircleBorder(), backgroundColor: black12),
+          //       child: Container(
+          //         width: 200,
+          //         height: 200,
+          //         alignment: Alignment.center,
+          //         decoration: const BoxDecoration(shape: BoxShape.circle),
+          //         child: Image.asset(imagePath, color: white),
           //       ),
+          //       onPressed: () {},
           //     ),
           //   ),
           // ),
